01b6af4d6d371ed5bb1f7a261a615e341217a7cf
damianhaq <40294540+damianhaq@users.noreply.github.com>
2023-08-13 17:39:00 +0200

    start template with basic animation

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 import pygame
 from sys import exit
+from spritesheet_data import spritesheet_data
+from functions import *
+
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
+
 pygame.display.set_caption("Nauka pygame")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
 
-elf = pygame.image.load("graphics/elf_m_idle_anim_f0.png")
+# text
+test_font = pygame.font.Font(None, 50)
 text_surface = test_font.render("My game", False, "Green")
 
+# elf
+elf = pygame.image.load("graphics/elf_m_idle_anim_f0.png").convert_alpha()
+big_sprite_small = pygame.image.load("graphics/BigSpritev7.png").convert_alpha()
+# big_sprite = pygame.transform.scale2x(big_sprite_small)
+width = big_sprite_small.get_width()
+height = big_sprite_small.get_height()
+scale = 3
+big_sprite = pygame.transform.scale(big_sprite_small, (width * scale, height * scale))
+
+counter = 0
 while True:
+    # counter
+    # print(counter)
+    counter += 0.1
+    if counter > 3:
+        counter = 0
+
+    # reset screen
+    screen.fill((50, 50, 50))
+
+    # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -18,6 +42,17 @@ while True:
 
     screen.blit(elf, (0, 0))
     screen.blit(text_surface, (50, 50))
+
+    screen.blit(
+        big_sprite,
+        (100, 100),
+        (
+            spritesheet_data["npc_1"]["idle"][int(counter)]["x"] * scale,
+            spritesheet_data["npc_1"]["idle"][int(counter)]["y"] * scale,
+            spritesheet_data["npc_1"]["idle"][int(counter)]["w"] * scale,
+            spritesheet_data["npc_1"]["idle"][int(counter)]["h"] * scale,
+        ),
+    )
 
     pygame.display.update()
     clock.tick(60)
---
+++ functions.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+def create_surface_array(spritesheet_data, spritesheet_image):
+    arr = []
+    for sprite in spritesheet_data:
+        print(sprite)
+
+        sur = pygame.Surface((sprite["w"], sprite["h"])).convert_alpha()
+        sur.blit(spritesheet_image, (0, 0), (0, 0, sprite["w"], sprite["h"]))
+        sur.set_colorkey((0, 0, 0))
+        arr.append(sur)
+
+    return arr
